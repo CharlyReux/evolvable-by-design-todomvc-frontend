cbd88b71719f2e658155916c0656ff050d49064c
charly reux <charlyreux@gmail.com>
2024-05-22 17:48:12 +0200

    Necessary changes for the remote dev to work

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.CODESPACE_NAME ? `https://${process.env.CODESPACE_NAME}-3000.app.github.dev` : "http://localhost:3000"
+const API_URL = process.env.CODESPACE_NAME ? `https://${process.env.CODESPACE_NAME}-8080.app.github.dev/rest` : "http://localhost:8080/rest"
 
 export default API_URL
